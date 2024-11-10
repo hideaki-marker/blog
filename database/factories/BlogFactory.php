8b9365b615e24c408e21c61eeff52a9f502f1b17
hideaki-marker-admin <sunday.slowlife@gmail.com>
2024-11-10 19:00:28 +0900

    データベースのダミーデータ挿入

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -17,7 +17,12 @@ class BlogFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-        ];
+            'title' => $this->faker->realText(10),
+            'content' => $this->faker->realText(60),
+            'image' => '',
+            'user_id' => $this->faker->numberBetween($min = 1, $max = 3),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]; 
     }
 }
